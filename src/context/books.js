761942da53df2ef437904f19c6659460c6a25598
Rochan <bahadur.rochan@gmail.com>
2021-10-27 17:52:40 +0530

    fetching from S3 added

--- src/context/books.js
+++ src/context/books.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { v4 as uuidv4 } from "uuid";
+import axios from "axios";
 import { listBooks } from "../api/queries";
 import { processOrder } from "../api/mutations";
 
@@ -8,6 +9,7 @@ const BookContext = React.createContext();
 const BookProvider = ({ children }) => {
   const [books, setBooks] = useState([]);
   const [featured, setFeatured] = useState([]);
+  const [bookImages, setBookImages] = useState([]);
   const [loading, setLoading] = useState(false);
 
   useEffect(() => {
@@ -29,40 +31,45 @@ const BookProvider = ({ children }) => {
   const fetchBooks = () => {
     try {
       setLoading(true);
-      const books = [
-        {
-          id: "1",
-          image:
-            "https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1312051518l/8686068.jpg",
-          title: "Devotion Of Suspect X",
-          author: "Keigo Higashino",
-          price: "15",
-          featured: true,
-          description:
-            "The Devotion of Suspect X is a 2005 novel by Keigo Higashino, the third in his Detective Galileo series and is his most acclaimed work thus far. The novel won him numerous awards, including the 134th Naoki Prize, which is a highly regarded award in Japan.",
-        },
-        {
-          id: "2",
-          image:
-            "https://images-na.ssl-images-amazon.com/images/I/51wtgYMGroL._SX321_BO1,204,203,200_.jpg",
-          title: "The Twilight of the Idols and The Anti-Christ",
-          price: "35",
-          featured: true,
-        },
-        {
-          id: "3",
-          image: "https://m.media-amazon.com/images/I/51iqpxo43PL.jpg",
-          title: "The Selfish Gene",
-          price: "20",
-          featured: true,
-        },
-      ];
-      const featured = books.filter((book) => {
-        return !!book.featured;
+      axios.get(`http://localhost:3200/images`).then((res) => {
+        const bookImagesData = res.data;
+        console.log(bookImagesData);
+        setBookImages(bookImagesData);
+
+        const books = [
+          {
+            id: "1",
+            title: "The Twilight of the Idols and The Anti-Christ",
+            price: "35",
+            featured: true,
+          },
+          {
+            id: "2",
+            title: "Devotion Of Suspect X",
+            author: "Keigo Higashino",
+            price: "15",
+            featured: true,
+            description:
+              "The Devotion of Suspect X is a 2005 novel by Keigo Higashino, the third in his Detective Galileo series and is his most acclaimed work thus far. The novel won him numerous awards, including the 134th Naoki Prize, which is a highly regarded award in Japan.",
+          },
+          {
+            id: "3",
+            title: "The Selfish Gene",
+            price: "20",
+            featured: true,
+          },
+        ];
+
+        for (let i = 0; i < books.length; i++) {
+          books[i]["image"] = "data:image/jpg;base64, " + bookImagesData[i];
+        }
+        const featured = books.filter((book) => {
+          return !!book.featured;
+        });
+        setBooks(books);
+        setFeatured(featured);
+        setLoading(false);
       });
-      setBooks(books);
-      setFeatured(featured);
-      setLoading(false);
     } catch (err) {
       console.log(err);
     }
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,30 +5,33 @@ import Hero from "../components/Hero";
 import { BookContext } from "../context/books";
 
 const Home = () => {
-    const { featured } = useContext(BookContext);
-    if (!featured.length) {
-        return <h3>No Featured Books</h3>
-    }
-    return (
-        <>
-            <Hero />
-            <section className="featured">
-                <header className="featured-head">
-                    <h3>Featured Collection</h3>
-                </header>
-                <div className="books featured-list">
-                    {featured.map(({ id, image, title }) => (
-                        <article key={id} className="book featured-book">
-                            <div className="book-image">
-                                <img src={image} alt={title} />
-                            </div>
-                            <Link to={`books/${id}`} className="btn book-link">details</Link>
-                        </article>
-                    ))}
-                </div>
-            </section>
-        </>
-    )
-}
+  const { featured } = useContext(BookContext);
+  if (!featured.length) {
+    return <h3>No Featured Books</h3>;
+  }
+  return (
+    <>
+      <Hero />
+      <section className="featured">
+        <header className="featured-head">
+          <h3>Featured Collection</h3>
+        </header>
+        <div className="books featured-list">
+          {featured.map(({ id, image, title }) => (
+            <article key={id} className="book featured-book">
+              <div className="book-image">
+                <img src={image} alt={title} />
+              </div>
+              <Link to={`books/${id}`} className="btn book-link">
+                details
+              </Link>
+            </article>
+          ))}
+        </div>
+      </section>
+    </>
+  );
+};
 
 export default Home;
+
